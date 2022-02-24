8bd056f1b3fca49fdbcd1b20c5899cc040a11ce1
Yiyun Qin <yiyun.qin@mths.ca>
2022-02-24 19:38:34 +0000

    math program

---
+++ math.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by Yiyun Qin
+# Created on February 2022
+# This is the math program, with proper style
+
+
+def main():
+    print("If a triangle has the dimentions:")
+    print("5cm x 3cm")
+    print("")
+    print("Area is {} cm².".format(5 * 3))
+    print("Perimetre is {} cm.".format(2 * (5 + 3)))
+
+
+if __name__ == "__main__":
+    main()
